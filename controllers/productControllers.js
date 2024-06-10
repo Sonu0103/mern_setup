6b282f070ccb43dc3b2f573e8165c80ff53aeb4a
Sonu0103 <sonuhp2002@gmail.com>
2024-06-10 09:55:17 +0545

    changes in controllers and routes

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -2,6 +2,7 @@
 const { get } = require("http");
 const productModels = require("../models/productModels");
 const path = require("path");
+const fs = require("fs");
 // const bcrypt = require("bcrypt");
 // 1. Creating User Function
 
@@ -91,8 +92,118 @@ const getAllProducts = async (req, res) => {
   }
 };
 
+// fetch single product
+const getProduct = async (req, res) => {
+  // receive id from url
+  const productId = req.params.id;
+  try {
+    const product = await productModels.findById(productId);
+    res.status(201).json({
+      success: true,
+      message: "Product Fetched!",
+      product: product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+// Delete product
+const deleteProduct = async (req, res) => {
+  // get product id
+  const productId = req.params.id;
+  try {
+    // fetch all product
+
+    await productModels.findByIdAndDelete(productId);
+    res.status(201).json({
+      success: true,
+      message: "deleted",
+      // updatedProductList
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "internal server error",
+    });
+  }
+};
+
+// update product
+// 1. get a update id
+// 2. if new image is provided
+// 3. Upload (public)
+// 4. Delete old image - delete product
+// 5. update products
+
+const updateProduct = async (req, res) => {
+  try {
+    // if there is file, uplaod new & delete old
+    if (req.files && req.files.productImage) {
+      // upload new to /public/products
+      // 1. Destructure file
+      const { productImage } = req.files;
+
+      // make a new image name
+      // 1. Generating unique name for each file
+      const imageName = `${Date.now()}-${productImage.name}`;
+      // 2. Define Specific Path
+      const imageUploadPath = path.join(
+        __dirname,
+        `../public/products/${imageName}`
+      );
+      // move to folder
+      await productImage.mv(imageUploadPath);
+      // replace productImage name to new name
+      req.body.productImage = imageName;
+
+      //  # delete old image
+      // file product information (We gave only ID)
+      const existingProduct = await productModels.findById(req.params.id);
+
+      // Search that image in directory
+      if (req.body.productImage) {
+        // if new image is uploaded, then only remove old image
+        const oldImagePath = path.join(
+          __dirname,
+          `../public/products/${existingProduct.productImage}`
+        );
+        // delete from file system
+        fs.unlinkSync(oldImagePath);
+      }
+      // update in database
+      const updatedProduct = await productModels.findByIdAndUpdate(
+        req.params.id,
+        req.body
+      );
+
+      // send a RESPONSE
+      res.status(201).json({
+        success: true,
+        message: "Product Updated",
+        updatedProduct: updatedProduct,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: " internal server error",
+      error: error,
+    });
+  }
+};
+
 //exporting
 module.exports = {
   createProduct,
   getAllProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,5 +10,15 @@ router.post("/create", productControllers.createProduct);
 // fetch all
 router.get("/get_all_products", productControllers.getAllProducts);
 
+// fetch single product
+// if POST, body(data)
+router.get("/get_single_product/:id", productControllers.getProduct);
+
+// delete product
+router.delete("/delete_product/:id", productControllers.deleteProduct);
+
+// update product
+router.put("/update_product/:id", productControllers.updateProduct);
+
 //exporting
 module.exports = router;
