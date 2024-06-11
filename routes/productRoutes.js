5ce79c7cd1f094fa438f1432a438264f5b11152b
Sonu0103 <sonuhp2002@gmail.com>
2024-06-11 22:30:31 +0545

    changes in controllers

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,23 +2,30 @@
 
 const router = require("express").Router();
 const productControllers = require("../controllers/productControllers");
+const { authGuard, adminGuard } = require("../middleware/authGuard");
 
 //Make a create user API
 
 router.post("/create", productControllers.createProduct);
 
 // fetch all
-router.get("/get_all_products", productControllers.getAllProducts);
+// http:localhost:5000/api/product/get_all_products
+router.get(
+  "/get_all_products",
+  authGuard,
+
+  productControllers.getAllProducts
+);
 
 // fetch single product
 // if POST, body(data)
-router.get("/get_single_product/:id", productControllers.getProduct);
+router.get("/get_single_product/:id", authGuard, productControllers.getProduct);
 
 // delete product
-router.delete("/delete_product/:id", productControllers.deleteProduct);
+router.delete("/delete_product/:id", adminGuard, productControllers.deleteProduct);
 
 // update product
-router.put("/update_product/:id", productControllers.updateProduct);
+router.put("/update_product/:id", adminGuard, productControllers.updateProduct);
 
 //exporting
 module.exports = router;
